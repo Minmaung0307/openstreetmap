50bfc845bf5edc183891c09d966a99236c613c4f
Min Maung <minmaung0307@gmail.com>
2025-10-05 18:27:54 -0400

    updated sw

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,27 @@
-const CACHE='mmapp-v1';const ASSETS=['/','/index.html','/style.css','/app.js','/manifest.webmanifest','/images/event-placeholder.jpg','/events-mm.json'];self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
+const CACHE = "mmapp-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/manifest.webmanifest",
+  "/images/event-placeholder.jpg",
+  "/events-mm.json",
+];
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(
+          keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))
+        )
+      )
+  );
+});
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+});
